7fcafdb5114936e529ce1a12948d4ce3adc897a8
uRyanxD <72111090+uRyanxD@users.noreply.github.com>
2021-04-17 15:15:42 -0300

    Scripts cleanup

---
+++ scripts/upstreamMerge.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+PS1="$"
+basedir="$(cd "$1" && pwd -P)"
+
+function update {
+    cd "$basedir/$1"
+    git fetch && git reset --hard origin/master
+    cd ../
+    git add $1
+}
+
+update Bukkit
+update CraftBukkit
